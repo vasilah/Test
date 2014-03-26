865efa41833109805dcb398612a6816df8ed9e6c
vasilah <vasilah@gmail.com>
2014-03-26 18:12:44 +0600

    Добавил новое сообщение

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("123");
+
+            Console.WriteLine("457");
         }
     }
 }
